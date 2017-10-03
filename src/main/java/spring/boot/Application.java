a13a3472f0c972b24869aef09eb4dea2cb007675
alexj01 <janus.alex@gmail.com>
2017-10-02 21:15:55 -0400

    first commit

---
+++ src/main/java/spring/boot/Application.java
@@ -0,0 +1,26 @@
+package spring.boot;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/hello")
+    String hello() {
+        return "hola";
+    }
+}
